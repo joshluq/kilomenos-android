cd0d283626a487d5326e14d22b875ea3002dd664
Josh Luque <josh.luque@gmail.com>
2026-08-16 20:12:58 +0200

    Optimize imports in `AuthRepositoryImpl` by removing the unused `flowOf` import.

--- app/src/main/java/es/joshluq/kmsafe/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/es/joshluq/kmsafe/data/repository/AuthRepositoryImpl.kt
@@ -31,7 +31,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
