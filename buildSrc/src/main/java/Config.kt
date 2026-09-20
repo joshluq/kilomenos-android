b9f7149b334793749d58015dcb4bbb1e4ba135ed
Josh Luque <josh.luque@gmail.com>
2026-09-20 21:09:47 +0200

    Bump application version to 0.1.5 and version code to 7.

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -16,8 +16,8 @@ interface Environment {
 
 object AppConfig : Config {
     override val applicationNamespace: String = "es.joshluq.kmsafe"
-    override val versionCode: Int = 6
-    override val versionName: String = "0.1.4"
+    override val versionCode: Int = 7
+    override val versionName: String = "0.1.5"
 
     object Environments {
         object Development : Environment {
